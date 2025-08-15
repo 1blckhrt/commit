ece5bdd8c24a2fb4ab0c3260e214cb0a2dadb5c0
1blckhrt <williams.1691@wright.edu>
2025-08-15 08:40:58 -0400

    feat: introduce commit handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from formatter import format_commit_message
 from prompts import get_commit_type, get_description, get_scope
+import questionary
+import subprocess
 
 
 def main():
@@ -8,7 +10,15 @@ def main():
     description = get_description()
 
     commit_message = format_commit_message(commit_type, scope, description)
-    print(commit_message)
+
+    print(f"Generated commit message: \n{commit_message}")
+    print("Showing your git status...")
+    subprocess.run(["git", "status"])
+
+    if questionary.confirm("Do you want to use this commit message?").ask():
+        subprocess.run(["git", "commit", "-m", commit_message])
+    else:
+        print("Commit message discarded.")
 
 
 if __name__ == "__main__":
